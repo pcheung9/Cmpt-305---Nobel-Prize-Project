aca1b403bda5eeaa7d17556600e273541c13ae7f
pcheung9 <peteycheung@gmail.com>
2016-11-20 16:51:05 -0700

    Added comment

--- NobelPrize.java
+++ NobelPrize.java
@@ -1,7 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Group 9: Alex Neilson, Peter Cheung, Steven Letourneau
+ * CMPT305: Nobel Prize Project
+ * Instructor: Indratmo
  */
 package nobelprize;
 
@@ -29,6 +28,7 @@ import javafx.stage.Stage;
 
 public class NobelPrize extends Application {
     //@FXML
+    
     private Button searchButton; 
     private final Label firstName = new Label("First name:");
     private final Label lastName = new Label("Last name:");
